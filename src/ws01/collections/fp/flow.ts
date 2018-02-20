35f596d0aae5e1726405c48a575e7c840e28ac82
Michał Knapik <michal.knapik@u2i.com>
2018-02-20 13:19:27 +0100

    Allow flow to work with any function

--- src/ws01/collections/fp/flow.ts
+++ src/ws01/collections/fp/flow.ts
@@ -1,16 +1,20 @@
-import { identity, List, Mapping, Reducing } from '../common'
+import { identity } from '../common'
+
+type Transform<A, B> =  (a: A) => B
 
 // :mózg: https://github.com/Microsoft/TypeScript/issues/5453
-export function flow<A, B> (a: Mapping<A, B>): (collection: List<A>) => List<B>
-export function flow<A, B, C> (a: Mapping<A, B>, b: Mapping<B, C>): (collection: List<A>) => List<C>
-export function flow<A, B, C, D> (a: Mapping<A, B>, b: Mapping<B, C>, c: Mapping<C, D>): (collection: List<A>) => List<D>
-export function flow<A, B, C, D, E> (a: Mapping<A, B>, b: Mapping<B, C>, c: Mapping<C, D>, d: Mapping<D, E>): (collection: List<A>) => List<E>
-export function flow<A, B, C, D, E> (a: Mapping<A, B>, b?: Mapping<B, C>, c?: Mapping<C, D>, d?: Mapping<D, E>): (collection: List<A>) => List<B | C | D | E> {
+export function flow<A, B> (a: Transform<A, B>): (a: A) => B
+export function flow<A, B, C> (a: Transform<A, B>, b: Transform<B, C>): (a: A) => C
+export function flow<A, B, C, D> (a: Transform<A, B>, b: Transform<B, C>, c: Transform<C, D>): (a: A) => D
+export function flow<A, B, C, D, E> (a: Transform<A, B>, b: Transform<B, C>, c: Transform<C, D>, d: Transform<D, E>): (a: A) => E
+export function flow<A, B, C, D, E> (
+  a: Transform<A, B>, b?: Transform<B, C>, c?: Transform<C, D>, d?: Transform<D, E>
+): Transform<A, B | C | D | E> {
   // return collection => [a, b, c, d].reduce((acc, e) => {
   //   return ((e || identity) as any)(acc)
   // }, collection)
-  return (collection: List<A>) => {
-    const t0 = a(collection)
+  return (first: A) => {
+    const t0 = a(first)
     if (b === undefined)
       return t0
     const t1 = b(t0)
@@ -22,18 +26,16 @@ export function flow<A, B, C, D, E> (a: Mapping<A, B>, b?: Mapping<B, C>, c?: Ma
 }
 
 interface flow2 {
-  <A, B> (a: Mapping<A, B>): (collection: List<A>) => List<B>
+  <A, B> (a: Transform<A, B>): Transform<A, B>
 
-  <A, B, C> (a: Mapping<A, B>, b: Mapping<B, C>): (collection: List<A>) => List<C>
+  <A, B, C> (a: Transform<A, B>, b: Transform<B, C>): Transform<A, C>
 
-  <A, B, C> (a: Mapping<A, B>, b: Reducing<B, C>): (collection: List<A>) => C
+  <A, B, C, D> (a: Transform<A, B>, b: Transform<B, C>, c: Transform<C, D>): Transform<A, D>
 
-  <A, B, C, D> (a: Mapping<A, B>, b: Mapping<B, C>, c: Mapping<C, D>): (collection: List<A>) => List<D>
-
-  <A, B, C, D, E> (a: Mapping<A, B>, b: Mapping<B, C>, c: Mapping<C, D>, d: Mapping<D, E>): (collection: List<A>) => List<E>
+  <A, B, C, D, E> (a: Transform<A, B>, b: Transform<B, C>, c: Transform<C, D>, d: Transform<D, E>): Transform<A, E>
 }
 
-export const flow2: flow2 = function (...mappers: Mapping<any, any>[]) {
+export const flow2: flow2 = function (...mappers: Transform<any, any>[]) {
   return (collection: any) => mappers.reduce((acc, e) => {
     return ((e || identity) as any)(acc)
   }, collection)
