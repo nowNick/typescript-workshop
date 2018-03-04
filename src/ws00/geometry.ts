860cb9ada1037f524524d7728130ced680008b3e
Michał Knapik <michal.knapik@u2i.com>
2018-03-04 22:00:30 +0100

    add geometry tests

--- src/ws00/geometry.ts
+++ src/ws00/geometry.ts
@@ -1,32 +1,18 @@
-type Point = [number, number, number]
-type Vector = [number, number, number]
+export type Point = [number, number, number]
+export type Vector = [number, number, number]
 
-function distance0 (a: Point, b: Point) {
+export function distance (a: Point, b: Point) {
   const [xa, ya, za] = a
   const [xb, yb, zb] = b
   return Math.sqrt((xa - xb) ** 2 + (ya - yb) ** 2 + (za - zb) ** 2)
 }
 
-const a: [number, number, number] = [0, 0, 0]
-const b = [3, 3, 3]
-const c: Point = [5, 5, 5]
-const d: Vector = [2, 2, 2]
 
-// console.log(distance0(a, b))
-console.log(distance0(a, c))
-console.log(distance0(a, d))
-console.log(distance0(a, [2, 3, 4]))
-
-function vector (a: Point, b: Point): Vector {
+export function vector (a: Point, b: Point): Vector {
   return [a[0] - b[0], a[1] - b[1], a[2] - b[2]]
 }
 
-/// Because of structural typing Vector and Point are the same.
-// const v0 = vector(a, b)
-const v1 = vector(a, c)
-const v2 = vector(a, d)
-
-function add (...vs: Vector[]): Vector {
+export function add (...vs: Vector[]): Vector {
   return vs.reduce(([x0, y0, z0], v) => {
     const [x1, y1, z1] = v
 
---
+++ test/ws00/geometry.test.ts
@@ -0,0 +1,58 @@
+///<reference types="jest" />
+import { add, distance, Point, Vector, vector } from '@ws00/geometry'
+
+const context = describe
+
+describe('vector', () => {
+  describe('when points are the same', () => {
+    it('builds vector from points', () => {
+      const a: [number, number, number] = [0, 0, 0]
+      const b: Point = [0, 0, 0]
+      expect(vector(a, b)).toEqual([0, 0, 0])
+    })
+  })
+
+  describe('when points are not the same', () => {
+
+    it('builds vector from points', () => {
+      const a: [number, number, number] = [0, 0, 0]
+      const b: Point = [3, 3, 3]
+      expect(vector(a, b)).toEqual([-3, -3, -3])
+    })
+  })
+})
+
+describe('distance', () => {
+  describe('when points are the same', () => {
+    it('distance is 0', () => {
+      const a: [number, number, number] = [0, 0, 0]
+      const b: Point = [0, 0, 0]
+
+      // expect(distance(a, b)).toEqual(0)
+      expect(distance(a, b)).toEqual(0)
+    })
+  })
+
+  describe('when points are not the same', () => {
+    it('distance is not 0', () => {
+      const a: [number, number, number] = [0, 0, 0]
+      const b = [3, 3, 3]
+      const c: Point = [3, 3, 3]
+
+      // expect(distance(a, b)).toEqual(0)
+      expect(distance(a, c)).toEqual(5.196152422706632)
+    })
+  })
+})
+
+describe('add', () => {
+  it('adds all vectors', () => {
+    const a: [number, number, number] = [0, 0, 0]
+    const b: Point = [3, 3, 3]
+    const v0 = vector(a, b)
+    const v1: [number, number, number] = [1, 2, 3]
+    const v2: Vector = [2,3,4]
+
+    expect(add(v0, v1, v2)).toEqual([0, 2, 4])
+  })
+})
