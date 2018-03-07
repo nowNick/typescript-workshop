5b5410b43502ce9bd0ad22f1eb62541dbf448a00
Michał Knapik <michal.knapik@u2i.com>
2018-03-07 17:02:18 +0100

    Add collection tests

--- test/ws01/collections.test.ts
+++ test/ws01/collections.test.ts
@@ -7,19 +7,25 @@ const isEven = (n: number) => n % 2 === 0
 const sum = (a: number, b: number, idx: number) => a + b
 
 describe('map', () => {
-  describe('when mapping numbers', () => {
+  describe('when array is not empty', () => {
     it('maps all values', () => {
       const collection = [1, 1, 2, 3, 5, 8]
       const pow = (n: number) => n * n
       expect(map(collection, pow)).toEqual([1, 1, 4, 9, 25, 64])
     })
 
-    it('carry on types', () => {
+    it('carries on types', () => {
       const collection = [1, 1, 2, 3, 5, 8]
       const even = map(collection, isEven)
       const strs = map(even, n => n.toString())
       const lens = map(strs, s => s.length)
       expect(lens).toEqual([5, 5, 4, 5, 5, 4])
+    })
+  })
+
+  context('when array is empty', () => {
+    it('returns empty array', () => {
+      expect(map([], () => {})).toEqual([])
     })
   })
 })
@@ -33,6 +39,7 @@ describe('filter', () => {
       })
     })
   })
+
   context('when the collection is empty', () => {
     it('does nothing', () => {
       expect(filter([], isEven)).toEqual([])
