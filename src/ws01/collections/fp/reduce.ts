cceadc8a25f258efaa9ed2ee550364d4ba84817c
Michał Knapik <michal.knapik@u2i.com>
2018-05-24 20:12:49 +0200

    fix if truthy

--- src/ws01/collections/fp/reduce.ts
+++ src/ws01/collections/fp/reduce.ts
@@ -4,7 +4,7 @@ import { List, Reducer, Reducing } from '../common'
 function reduce<T> (reducer: Reducer<T, T>): Reducing<T, T>
 function reduce<T, K> (reducer: Reducer<T, K>, initial: K): Reducing<T, K>
 function reduce<T, K> (reducer: Reducer<T, K | T>, initial?: K) {
-  if (initial) {
+  if (initial !== undefined) {
     return (collection: List<T>) => ireduce(collection, reducer, initial)
   } else {
     return (collection: List<T>) => ireduce(collection, reducer as any as Reducer<T, T>)
