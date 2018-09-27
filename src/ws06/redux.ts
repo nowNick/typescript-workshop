17955977a4ac66404e0af0d989eecbaee8dcfdab
Michał Knapik <michal.knapik@u2i.com>
2018-09-27 11:02:47 +0200

    Redux: remove listeners

--- src/ws06/redux.ts
+++ src/ws06/redux.ts
@@ -8,38 +8,20 @@ export interface Dispatch<D = Action> {
   <A extends D>(action: A): A
 }
 
-export interface Unsubscribe {
-  (): void;
-}
-
 export interface Store<S = any, A extends Action = Action> {
   dispatch: Dispatch<A>
 
   getState(): S
-
-  subscribe(listener: () => void): Unsubscribe
 }
-
-type Listener = () => void
 
 export function createStore<S, A extends Action>(reducer: Reducer<S, A>, initialState: S | undefined): Store<S, A> {
   let state  = initialState
-  const listeners = new Map<number, Listener>()
-  let idSeq = 0
 
   return {
     getState: (): S => state as S,
     dispatch: <B extends A>(action: B) => {
       state = reducer(state, action)
       return action
-    },
-    subscribe: (listener: Listener) => {
-      const id = idSeq++;
-      listeners.set(id, listener)
-
-      return () => {
-        listeners.delete(id)
-      }
     }
   }
 }
