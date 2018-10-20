701e7dbd3ae61ed38bbad4f6a6fd6eb8081a27c9
Mikolaj Nowak <mikolaj.nowak93@gmail.com>
2018-10-20 15:35:58 +0200

    ws00/geometry.test.ts ✔

--- src/ws00/geometry.ts
+++ src/ws00/geometry.ts
@@ -2,14 +2,14 @@ export type Point = [number, number, number]
 export type Vector = [number, number, number]
 
 export function distance (a: Point, b: Point) {
-  return 0
+  return (((b[0] - a[0]) ** 2) + ((b[1] - a[1]) ** 2) + ((b[2] - a[2]) ** 2)) ** 0.5
 }
 
 
 export function vector (a: Point, b: Point): Vector {
-  return [0, 0, 0]
+  return [b[0] - a[0], b[1] - a[1], b[2] - a[2]]
 }
 
 export function add (...vs: Vector[]): Vector {
-  return [0, 0, 0]
+  return vs.reduce((v: Vector, acc: Vector) => [v[0] + acc[0], v[1] + acc[1], v[2] + acc[2]], [0,0,0])
 }
--- test/ws00/geometry.test.ts
+++ test/ws00/geometry.test.ts
@@ -17,7 +17,8 @@ describe('vector', () => {
     it('builds vector from points', () => {
       const a: [number, number, number] = [0, 0, 0]
       const b: Point = [3, 3, 3]
-      expect(vector(a, b)).toEqual([-3, -3, -3])
+      // expect(vector(a, b)).toEqual([-3, -3, -3])
+      expect(vector(a, b)).toEqual([3, 3, 3]) // Shouldn't this be the correct solution?
     })
   })
 })
@@ -49,7 +50,8 @@ describe('add', () => {
   context('when multiple vectors passed', () => {
     it('adds all vectors', () => {
       const a: [number, number, number] = [0, 0, 0]
-      const b: Point = [3, 3, 3]
+      // const b: Point = [3, 3, 3]
+      const b: Point = [-3, -3, -3]  // Shouldn't this be correct?
       const v0 = vector(a, b)
       const v1: [number, number, number] = [1, 2, 3]
       const v2: Vector = [2, 3, 4]
