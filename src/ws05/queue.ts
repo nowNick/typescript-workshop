3962b5c478424b919deb99224860cd4058a64e64
Mikolaj Nowak <mikolaj.nowak93@gmail.com>
2018-10-31 08:19:26 +0100

    ws05/*.test.ts ✔

--- src/ws05/queue.ts
+++ src/ws05/queue.ts
@@ -7,4 +7,17 @@ export interface Queue<T> {
 }
 
 export class Queue<T> implements Queue<T> {
+  elements: Array<T>
+
+  constructor() {
+    this.elements = []
+  }
+
+  pop() {
+    return this.elements.shift()
+  }
+
+  push(item: T) {
+    this.elements.push(item)
+  }
 }
--- src/ws05/stack.ts
+++ src/ws05/stack.ts
@@ -4,4 +4,17 @@ export interface Stack<T> {
 }
 
 export class Stack<T> implements Stack<T> {
+  elements: Array<T>
+
+  constructor() {
+    this.elements = []
+  }
+
+  push(item: T) {
+    this.elements.push(item)
+  }
+
+  pop() {
+    return this.elements.pop()
+  }
 }
